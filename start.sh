f7a902255c31b3f20f68f8f19363e5860814d362
Csepely Péter <csepely.peter@arosakft.hu>
2020-04-22 11:02:44 +0200

    conf and start.sh updates

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if [ -e /tmp/logpipe ]
-then
-  rm /tmp/logpipe
-fi
-mkfifo -m 600 /tmp/logpipe
-cat <> /tmp/logpipe 1>&2 &
-chown www-data /tmp/logpipe
-exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf 2>&1
+chmod a+w /dev/pts/0
+exec /usr/sbin/lighttpd -D -f /etc/lighttpd/lighttpd.conf
